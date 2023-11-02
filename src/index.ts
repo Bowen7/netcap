94e099f8c749c6441a31a1e8a0fd3a663e4d7e3f
Bowen7 <zwxdyx@foxmail.com>
2023-11-02 22:30:20 +0800

    record webpage to video

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import puppeteer from "puppeteer";
-import fs from "node:fs";
+import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
+import ffmpeg from "fluent-ffmpeg";
+import { PassThrough } from "node:stream";
+
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 const width = 1920;
 const height = 1080;
@@ -9,30 +13,17 @@ function later(delay) {
 }
 
 export const run = async () => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: [
-      //   "--no-sandbox",
-      //   // "--start-fullscreen",
-      //   "--disable-gpu",
-      `--window-size=${width},${height}`,
-      //   "--disable-setuid-sandbox",
-      //   `--ozone-override-screen-size=${width},${height}`,
-      //   "--headless=new",
-      "--start-maximized",
-    ],
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.setViewport({ width, height, deviceScaleFactor: 1 });
-  console.log(page.viewport());
+  const stream = new PassThrough();
   await page.goto("https://developer.mozilla.org/en-US/docs/Web/CSS/animation");
 
   const client = await page.target().createCDPSession();
 
   client.on("Page.screencastFrame", async (frameObject) => {
-    // Do what you want with frame, ex write to file on disk
     const buff = Buffer.from(frameObject.data, "base64");
-    fs.writeFileSync(Date.now() + ".png", buff);
+    stream.write(buff);
     await client.send("Page.screencastFrameAck", {
       sessionId: frameObject.sessionId,
     });
@@ -51,4 +42,32 @@ export const run = async () => {
 
   await client.send("Page.stopScreencast");
   await browser.close();
+
+  stream.end();
+
+  ffmpeg({
+    source: stream,
+    priority: 20,
+  })
+    .videoCodec("libx264")
+    .size("100%")
+    .aspect("4:3")
+    // .autopad(this.autopad.activation, this.autopad?.color)
+    .inputFormat("image2pipe")
+    .inputFPS(30)
+    .videoCodec("libx264")
+    .output("./dist/output.mp4")
+    .on("end", function () {
+      console.log("File has been converted succesfully");
+    })
+    .on("error", function (err) {
+      console.log("An error occurred: " + err.message);
+    })
+    .on("start", function (commandLine) {
+      console.log("Spawned FFmpeg with command: " + commandLine);
+    })
+    .on("progress", function (progress) {
+      console.log("processing: " + JSON.stringify(progress) + "% done");
+    })
+    .run();
 };
