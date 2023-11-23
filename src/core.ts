d5951ce075525b7e5301c1a7addc629b42868c2b
Bowen7 <zwxdyx@foxmail.com>
2023-11-23 23:40:20 +0800

    record screen

--- src/core.ts
+++ src/core.ts
@@ -1,44 +1,86 @@
-import { type Page } from 'puppeteer'
+import { type Page, type CDPSession } from 'puppeteer'
 import ffmpeg from 'fluent-ffmpeg'
 import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg'
 import { PassThrough } from 'node:stream'
 
 ffmpeg.setFfmpegPath(ffmpegPath)
 
+interface Options {
+  width?: number
+  height?: number
+}
+
+const DEFAULT_OPTIONS: Options = {
+  width: 1920,
+  height: 1080
+}
+
 export class NetCap {
   private status: 'pending' | 'recording' | 'paused' = 'pending'
   page: Page
+  options: Options
+  client: CDPSession | null = null
   pass: PassThrough
-  constructor(page: Page) {
+  constructor(page: Page, options: Options = {}) {
     this.page = page
-    this.pass = new PassThrough()
+    this.options = { ...DEFAULT_OPTIONS, ...options }
   }
 
-  start(): void {
+  private async startScreencast(): Promise<void> {
+    const { width, height } = this.options
+    return await this.client?.send('Page.startScreencast', {
+      format: 'jpeg',
+      quality: 100,
+      maxWidth: width,
+      maxHeight: height,
+      everyNthFrame: 1
+    })
+  }
+
+  async start(): Promise<void> {
     if (this.status !== 'pending') {
       console.error('NetCap is already started')
     }
     this.status = 'recording'
+    if (this.client !== null) {
+      this.client = await this.page.target().createCDPSession()
+    }
+    this.pass = new PassThrough()
+    this.client?.on('Page.screencastFrame', (frameObject) => {
+      const buff = Buffer.from(frameObject.data, 'base64')
+      this.pass.write(buff)
+      this.client
+        ?.send('Page.screencastFrameAck', {
+          sessionId: frameObject.sessionId
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    })
+    await this.startScreencast()
   }
 
-  stop(): void {
+  async stop(): Promise<void> {
     if (this.status !== 'recording') {
       console.error('NetCap is not recording')
     }
     this.status = 'pending'
+    await this.client?.send('Page.stopScreencast')
   }
 
-  pause(): void {
+  async pause(): Promise<void> {
     if (this.status !== 'recording') {
       console.error('NetCap is not recording')
     }
     this.status = 'paused'
+    await this.client?.send('Page.stopScreencast')
   }
 
-  resume(): void {
+  async resume(): Promise<void> {
     if (this.status !== 'paused') {
       console.error('NetCap is not paused')
     }
     this.status = 'recording'
+    await this.startScreencast()
   }
 }
