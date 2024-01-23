62145a56b67552fab2f8f39f0181e81e8a2ed336
Bowen7 <zwxdyx@foxmail.com>
2024-01-23 22:29:11 +0800

    receive cursorScale as a param

--- src/preload/mouse.ts
+++ src/preload/mouse.ts
@@ -2,6 +2,9 @@
 import { type Page } from 'puppeteer'
 import { type Cursors } from '../types'
 
+export interface CursorOptions {
+  cursorScale?: number
+}
 declare global {
   interface Window {
     _cursor_move_finished: boolean
@@ -66,120 +69,137 @@ const cursors: Cursors = {
     offset: [8, 3]
   }
 }
-
+interface CursorFuncArgs {
+  cursors: Cursors
+  cursorScale: number
+}
 // ScreenCast will not record the cursor, so we need to mock it.
-export const enableMouse = async (page: Page): Promise<void> => {
+export const enableCursor = async (
+  page: Page,
+  options: CursorOptions = {}
+): Promise<void> => {
   // page.on('console', (log) => {
   //   console.log(log.text())
   // })
-  await page.evaluateOnNewDocument((cursors: Cursors) => {
-    const isPointInRect = (point: [number, number], rect: DOMRect): boolean => {
-      const [x, y] = point
-      const { left, right, top, bottom } = rect
-      return x > left && x < right && y > top && y < bottom
-    }
-
-    const moveCursor = (
-      cursorContainer: HTMLDivElement,
-      event: MouseEvent
-    ): void => {
-      const { clientX: x, clientY: y } = event
-      cursorContainer.style.left = `${x}px`
-      cursorContainer.style.top = `${y}px`
-    }
-
-    const getCursorType = (event: MouseEvent): string => {
-      let cursorType = 'default'
-      const target = event.target as HTMLElement
-      if (!target) {
-        return cursorType
+  const { cursorScale = 1 } = options
+  await page.evaluateOnNewDocument(
+    ({ cursors, cursorScale }: CursorFuncArgs) => {
+      const isPointInRect = (
+        point: [number, number],
+        rect: DOMRect
+      ): boolean => {
+        const [x, y] = point
+        const { left, right, top, bottom } = rect
+        return x > left && x < right && y > top && y < bottom
       }
-      cursorType =
-        target.style.cursor || getComputedStyle(target).cursor || 'default'
-      // If `cursorType` is 'auto', we should further detect the cursor style.
-      if (cursorType === 'auto') {
-        cursorType = 'default'
-        try {
-          const { clientX: x, clientY: y } = event
-          const range = document.caretRangeFromPoint(x, y)
-          if (range) {
-            const textNode = range.startContainer
-            if (textNode?.nodeType === Node.TEXT_NODE) {
-              // If the cursor is in the blank space of a text node (such as word wrap or the end of a paragraph), it should not be the 'text' cursor.
-              const index = range.startOffset
-              range.setStart(textNode, index === 0 ? 0 : index - 1)
-              range.setEnd(textNode, index + 1)
-              const rects = range.getClientRects()
-              const isPointInRects = Array.from(rects).some((rect) =>
-                isPointInRect([x, y], rect)
-              )
-              if (isPointInRects) {
-                cursorType = 'text'
+
+      const moveCursor = (
+        cursorContainer: HTMLDivElement,
+        event: MouseEvent
+      ): void => {
+        const { clientX: x, clientY: y } = event
+        cursorContainer.style.left = `${x}px`
+        cursorContainer.style.top = `${y}px`
+      }
+
+      const getCursorType = (event: MouseEvent): string => {
+        let cursorType = 'default'
+        const target = event.target as HTMLElement
+        if (!target) {
+          return cursorType
+        }
+        cursorType =
+          target.style.cursor || getComputedStyle(target).cursor || 'default'
+        // If `cursorType` is 'auto', we should further detect the cursor style.
+        if (cursorType === 'auto') {
+          cursorType = 'default'
+          try {
+            const { clientX: x, clientY: y } = event
+            const range = document.caretRangeFromPoint(x, y)
+            if (range) {
+              const textNode = range.startContainer
+              if (textNode?.nodeType === Node.TEXT_NODE) {
+                // If the cursor is in the blank space of a text node (such as word wrap or the end of a paragraph), it should not be the 'text' cursor.
+                const index = range.startOffset
+                range.setStart(textNode, index === 0 ? 0 : index - 1)
+                range.setEnd(textNode, index + 1)
+                const rects = range.getClientRects()
+                const isPointInRects = Array.from(rects).some((rect) =>
+                  isPointInRect([x, y], rect)
+                )
+                if (isPointInRects) {
+                  cursorType = 'text'
+                }
               }
             }
-          }
-        } catch (error) {}
-      }
-      return cursorType
-    }
-
-    const setCursorStyle = (
-      cursorContainer: HTMLDivElement,
-      cursorType: string
-    ): void => {
-      const cursor = cursors[cursorType] || cursors.default
-      const { image, offset = [0, 0] } = cursor
-      const [x, y] = offset
-      cursorContainer.innerHTML = image
-      cursorContainer.style.transform = `translate(-${x}px, -${y}px)`
-    }
-
-    const createCursorContainer = (): HTMLDivElement => {
-      const container = document.createElement('div')
-      container.style.position = 'absolute'
-      container.style.pointerEvents = 'none'
-      container.style.left = '-999px'
-      container.style.top = '-999px'
-      // max z-index
-      container.style.zIndex = '2147483647'
-      setCursorStyle(container, 'default')
-      document.body.appendChild(container)
-      return container
-    }
-
-    if (window === window.parent) {
-      window.addEventListener('DOMContentLoaded', () => {
-        let prevMouseEvent: MouseEvent | null = null
-        let latestMouseEvent: MouseEvent | null = null
-        let latestCursorType = 'default'
-
-        const container = createCursorContainer()
-
-        document.addEventListener(
-          'mousemove',
-          (event) => {
-            latestMouseEvent = event
-          },
-          true
-        )
-
-        const rafCallback = (): void => {
-          requestAnimationFrame(rafCallback)
-          if (latestMouseEvent && latestMouseEvent !== prevMouseEvent) {
-            window._cursor_move_finished = false
-            prevMouseEvent = latestMouseEvent
-            moveCursor(container, latestMouseEvent)
-            const cursorType = getCursorType(latestMouseEvent)
-            if (cursorType !== latestCursorType) {
-              setCursorStyle(container, cursorType)
-              latestCursorType = cursorType
-            }
-          } else {
-            window._cursor_move_finished = true
-          }
+          } catch (error) {}
         }
-        rafCallback()
-      })
-    }
-  }, cursors)
+        return cursorType
+      }
+
+      const setCursorStyle = (
+        cursorContainer: HTMLDivElement,
+        cursorType: string
+      ): void => {
+        const scale = cursorScale
+        const cursor = cursors[cursorType] || cursors.default
+        const { image, offset = [0, 0] } = cursor
+        const [offsetX, offsetY] = offset
+        const transformX = -scale * offsetX
+        const transformY = -scale * offsetY
+        cursorContainer.innerHTML = image
+        cursorContainer.style.transformOrigin = '0 0'
+        cursorContainer.style.transform = `translate(${transformX}px, ${transformY}px) scale(${scale})`
+      }
+
+      const createCursorContainer = (): HTMLDivElement => {
+        const container = document.createElement('div')
+        container.style.position = 'absolute'
+        container.style.pointerEvents = 'none'
+        container.style.left = '-999px'
+        container.style.top = '-999px'
+        // max z-index
+        container.style.zIndex = '2147483647'
+        setCursorStyle(container, 'default')
+        document.body.appendChild(container)
+        return container
+      }
+
+      if (window === window.parent) {
+        window.addEventListener('DOMContentLoaded', () => {
+          let prevMouseEvent: MouseEvent | null = null
+          let latestMouseEvent: MouseEvent | null = null
+          let latestCursorType = 'default'
+
+          const container = createCursorContainer()
+
+          document.addEventListener(
+            'mousemove',
+            (event) => {
+              latestMouseEvent = event
+            },
+            true
+          )
+
+          const rafCallback = (): void => {
+            requestAnimationFrame(rafCallback)
+            if (latestMouseEvent && latestMouseEvent !== prevMouseEvent) {
+              window._cursor_move_finished = false
+              prevMouseEvent = latestMouseEvent
+              moveCursor(container, latestMouseEvent)
+              const cursorType = getCursorType(latestMouseEvent)
+              if (cursorType !== latestCursorType) {
+                setCursorStyle(container, cursorType)
+                latestCursorType = cursorType
+              }
+            } else {
+              window._cursor_move_finished = true
+            }
+          }
+          rafCallback()
+        })
+      }
+    },
+    { cursors, cursorScale }
+  )
 }
